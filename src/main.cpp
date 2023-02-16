5358eb12cc5064e2a2a4bb50e2d22833b70a3f30
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2023-02-16 20:52:58 +0530

    Created first working prototype.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,15 @@
 #include <fstream>
 #include <string>
 
+class WordNode {
+	public:
+		long index;
+		std::vector<long> possible_words;
+		WordNode(long index, std::vector<long>&& possible_words)
+			: index(index), possible_words(possible_words)
+		{}
+};
+
 std::vector<std::string> get_word_list() {
 	std::ifstream file("res/words.txt");
 	std::vector<std::string> word_list;
@@ -21,8 +30,8 @@ std::vector<std::string> get_word_list() {
 	return word_list;
 }
 
-bool is_used(std::vector<std::string>& word_list, size_t index, std::array<long, 4> indexes) {
-	for (size_t i = 0; i < 4; i++) {
+bool is_used(std::vector<std::string>& word_list, size_t index, std::array<long, 3> indexes) {
+	for (size_t i = 0; i < 3; i++) {
 		int current_index = indexes[i];
 		if (current_index < 0) {
 			break;
@@ -45,17 +54,32 @@ bool is_used(std::vector<std::string>& word_list, size_t index, std::array<long,
 
 int main(int argc, char *argv[]) {
 	std::vector<std::string> word_list = get_word_list();
+	std::vector<WordNode> word_list_graph;
+	for (int i = 0; i < word_list.size(); i++) {
+		std::vector<long> indexes;
+		for (int j = i; j < word_list.size(); j++) {
+			if (!is_used(word_list, j, { i, -1, -1 })) {
+				indexes.push_back(j);
+			}
+		}
+		word_list_graph.push_back(WordNode(i, std::move(indexes)));
+	}
 	if (word_list.size() > 0) {
-		for (long a = 0; a < word_list.size(); a++) {
-			for (long b = a; b < word_list.size(); b++) {
-				if (is_used(word_list, b, { a, -1, -1, -1 })) continue;
-				for (long c = b; c < word_list.size(); c++) {
-					if (is_used(word_list, c, { a, b, -1, -1 })) continue;
-					for (long d = c; d < word_list.size(); d++) {
-						if (is_used(word_list, d, { a, b, c, -1 })) continue;
-						for (long e = d; e < word_list.size(); e++) {
-							if (!is_used(word_list, e, { a, b, c, d })) {
-								std::cout << word_list[a] << word_list[b] << word_list[c] << word_list[d] << word_list[e] << std::endl;
+		for (long a = 0; a < word_list_graph.size(); a++) {
+			auto& ap = word_list_graph[a];
+			std::cout << word_list[ap.index] << std::endl;
+			for (long b = 0; b < ap.possible_words.size(); b++) {
+				auto& bp = word_list_graph[ap.possible_words[b]];
+				for (long c = 0; c < bp.possible_words.size(); c++) {
+					auto& cp = word_list_graph[bp.possible_words[c]];
+					if (is_used(word_list, cp.index, { ap.index, -1, -1 })) continue;
+					for (long d = 0; d < cp.possible_words.size(); d++) {
+						auto& dp = word_list_graph[cp.possible_words[d]];
+						if (is_used(word_list, dp.index, { ap.index, bp.index, -1 })) continue;
+						for (long e = 0; e < dp.possible_words.size(); e++) {
+							auto& ep = word_list_graph[dp.possible_words[e]];
+							if (!is_used(word_list, ep.index, { ap.index, bp.index, cp.index })) {
+								std::cout << word_list[ap.index] << word_list[bp.index] << word_list[cp.index] << word_list[dp.index] << word_list[ep.index] << std::endl;
 							}
 						}
 					}
