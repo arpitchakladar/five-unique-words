c536aa7959f476d3b8833df9839a5183a7eaa97e
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2023-02-17 11:42:52 +0530

    Returning the list of words that need to be checked.

--- src/main.cpp
+++ src/main.cpp
@@ -24,15 +24,35 @@ std::vector<Word> get_word_list() {
 }
 
 template<size_t N>
-const std::vector<Word*>& get_most_limiting(std::vector<Word>& word_list, std::array<Word, N> words) {
-	const std::vector<Word*> *current_min = &words[0].get_next_words();
+class MostLimiting {
+	public:
+		const std::vector<Word*>& next_words;
+		std::array<Word*, N> remaining_words;
+
+		MostLimiting(const std::vector<Word*>& next_words, std::array<Word*, N>&& remaining_words)
+			: next_words(next_words), remaining_words(remaining_words)
+		{}
+};
+
+template<size_t N>
+MostLimiting<N - 1> get_most_limiting(std::vector<Word>& word_list, std::array<Word*, N> words) {
+	const std::vector<Word*> *current_min = &words[0]->get_next_words();
+	size_t x = 0;
 	for (size_t i = 0; i < N; i++) {
-		const std::vector<Word*>& current_list = words[i].get_next_words();
+		const std::vector<Word*>& current_list = words[i]->get_next_words();
 		if (current_list.size() < current_min->size()) {
 			current_min = &current_list;
+			x = i;
 		}
 	}
-	return *current_min;
+	std::array<Word*, N - 1> remaining_words;
+	for (size_t i = 0, j = 0; j < N; j++) {
+		if (j != x) {
+			remaining_words[i] = words[j];
+			i++;
+		}
+	}
+	return MostLimiting(*current_min, std::move(remaining_words));
 }
 
 int main(int argc, char *argv[]) {
@@ -43,6 +63,6 @@ int main(int argc, char *argv[]) {
 	std::cout << word_list[2].get_next_words().size() << std::endl;
 	std::cout << word_list[5].get_next_words().size() << std::endl;
 	std::cout << word_list[7].get_next_words().size() << std::endl;
-	std::cout << get_most_limiting(word_list, std::array { word_list[2], word_list[5], word_list[7] }).size() << std::endl;
+	std::cout << get_most_limiting(word_list, std::array { &word_list[2], &word_list[5], &word_list[7] }).next_words.size() << std::endl;
 	return EXIT_SUCCESS;
 }
